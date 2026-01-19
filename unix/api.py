31380bd51d3e8e11daf22bfefff27913551fa767
Dervin Tian <dervint@umich.edu>
2026-01-18 23:49:18 -0500

    Low Poly render with buildings and roads

---
+++ unix/api.py
@@ -0,0 +1,56 @@
+import requests
+import json
+
+# These represent the region of interest, and we can change them here or control them elsewhere using like a website or smth like before
+min_lat, min_lon = 42.2901, -83.7193
+max_lat, max_lon = 42.294, -83.7101
+
+# this is good for one-off buildings / sampling. should not be used for the actual pipeline
+OVERPASS_URL = "https://overpass-api.de/api/interpreter"
+# directly using the OSM API is not suitable for analysis, its more primitive
+
+# Next things would be to change query to get the roads as well as buildings
+# This is the overpass query format in order to get the data (ChatGPT this cuz i dunno the format)
+query_buildings = f"""
+[out:json][timeout:25];
+way["building"]({min_lat},{min_lon},{max_lat},{max_lon});
+out body geom;
+"""
+
+query_street = f"""
+[out:json][timeout:25];
+way["highway"]({min_lat},{min_lon},{max_lat},{max_lon});
+out body geom;
+"""
+
+# Do a post request (Chat says that post request works better with larger queries)
+r = requests.post(OVERPASS_URL, data=query_buildings)
+r.raise_for_status()
+data_buildings = r.json()
+# Convert to json format
+
+# Encode boundaries into the JSOn as well
+data_buildings["max_lat"] = max_lat
+data_buildings["min_lat"] = min_lat
+data_buildings["max_lon"] = max_lon
+data_buildings["min_lon"] = min_lon
+
+# Write to the JSON output
+with open("osm_data_buildings.json", "w") as f:
+    json.dump(data_buildings, f, indent=2)
+
+# Do a post request (Chat says that post request works better with larger queries)
+r = requests.post(OVERPASS_URL, data=query_street)
+r.raise_for_status()
+data_street = r.json()
+# Convert to json format
+
+# Encode boundaries into the JSOn as well
+data_street["max_lat"] = max_lat
+data_street["min_lat"] = min_lat
+data_street["max_lon"] = max_lon
+data_street["min_lon"] = min_lon
+
+# Write to the JSON output
+with open("osm_data_roads.json", "w") as f:
+    json.dump(data_street, f, indent=2)
---
+++ unix/extrude_out.py
@@ -0,0 +1,198 @@
+import shapely
+from shapely.geometry import Polygon
+from pyproj import Transformer
+import trimesh
+import numpy as np
+import json
+
+# Global, path to the outputted JSON file
+INPUT_JSON = "osm_data_buildings.json" 
+STREET_JSON = "osm_data_roads.json"
+SCALE = 5 # What level of precision we want
+
+# Will generate under the assumption that the starting_point is located in the top left corner
+def generate_plane(height, width):
+    # Need to generate the vertexes that are in the perimeter
+
+    # Just make the corners to the size of the bounding box
+    corners = [
+        [0, 0, 0],
+        [0, width, 0],
+        [height, width, 0],
+        [height, 0, 0],
+    ]
+
+    # Tells trimesh that I want a face represented by corners[0], corners[1], corners[2], and corners[3]
+    faces = np.array([[0, 1, 2, 3]])
+
+    # Generate the plane :)
+    plane = trimesh.Trimesh(vertices=corners, faces=faces)
+
+    return plane
+
+
+def initialize_plane(data):
+    # Rounding to a decimal place of 5 gives us around 1 m accuracy in the real world (worst case at equator), according to Chat
+    max_lat = int(MAX_LAT * (10**SCALE))
+    min_lat = int(MIN_LAT * (10**SCALE))
+    max_lon = int(MAX_LON * (10**SCALE))
+    min_lon = int(MIN_LON * (10**SCALE))
+
+    delta_lat = abs(max_lat - min_lat) # width of our underlying plane mesh
+    delta_long = abs(max_lon - min_lon) # height of our underlying plane mesh
+
+    plane = generate_plane(delta_lat, delta_long) # plane is a mesh
+    return plane
+    
+
+def get_corners(element):
+    # Go into the API to get the geometry lat, lon points (representative of the nodes)
+    geometry_points = element["geometry"]
+    corners = []
+
+    # Go through each geometry point, and take the lat and lon and convert it to x and y
+    for point in geometry_points:
+        # Convert the latitude and longitude into ints (multiplying by 100,000 to get rid of decimals)
+        latitude = int(float(point["lat"]) * (10**SCALE))
+        longitude = int(float(point["lon"]) * (10**SCALE))
+
+        # Calculate the offset relative to the min and max bounds of the region
+        local_i = abs(latitude - int(MIN_LAT * (10**SCALE)))
+        local_j = abs(longitude - int(MIN_LON * (10**SCALE)))
+
+        # Add the coordinates to the corners
+        corners.append([local_i, local_j])
+    
+    return corners
+
+# Get the trimesh lines that connects the corners together
+def get_lines(corners, loop=True):
+    lines = []
+    # Lines go from corners[start] to corners[end]
+    # Since Overpass already has the geometry nodes in order, we just need to loop from 0-1, 1-2, etc and then [end, 0] to wrap back to beginning, close the polygon
+    start = 0
+    end = 1
+    lines.append(trimesh.path.entities.Line([start, end])) # Add in the start of the path
+
+    # Go until we hit the end and add in those lines
+    for i in range(len(corners) - 2):
+        start += 1
+        end += 1
+        lines.append(trimesh.path.entities.Line([start, end]))
+
+    # Add in that last trimesh line that puts us back at the beginning (closes the polygon)
+    if loop:
+        lines.append(trimesh.path.entities.Line([end, 0]))
+    return lines
+
+def get_height(element):
+    # Some elements have a height attribute in their tags section
+    height = element["tags"].get("height") # try to get the height value
+    if not height:
+        # If there is no height, get the building levels tag
+        height = element["tags"].get("building:levels")
+
+        if not height:
+            # If there isn't any sort of building leves, just set the height to be one story, times the height of one story (3 meters?)
+            height = float(3 * 1)
+        else:
+            # Otherwise, set the height to be the number of stories * 3 meters
+            height = 3 * float(height)
+    else:
+        height = float(height)
+    
+    return height
+
+def get_width(element):
+    return 2
+
+
+def main():
+    # Just helps for debugging
+    trimesh.util.attach_to_log()
+    data_buildings = None
+    
+    # Read in the json data
+    with open(INPUT_JSON, "r") as f:
+        data_buildings = json.load(f)
+
+    # Declare the bounds as globals so that everyone can use them
+    global MAX_LAT
+    global MIN_LAT
+    global MAX_LON
+    global MIN_LON
+
+    # Set the global bounds
+    MAX_LAT = float(data_buildings["max_lat"])
+    MIN_LAT = float(data_buildings["min_lat"])
+    MAX_LON = float(data_buildings["max_lon"]) 
+    MIN_LON = float(data_buildings["min_lon"])
+    
+    # Initialize that initial plane, great name I know
+    hisodflkjas = initialize_plane(data_buildings)
+    hisodflkjas.visual.face_colors = [0, 255, 0, 255]
+    hisodflkjas.export("hisodflkjas.glb")
+
+    # buildings will be the combined mesh of everything in the scene, so the underlying plane, buildings, roads etc.
+    buildings = []
+    buildings.append(hisodflkjas) # Add in the plane to begin
+
+    # Go through the elements returned by overpass' API
+    for i, element in enumerate(data_buildings["elements"]):
+
+        # Get the corners
+        corners = get_corners(element)
+
+        # Get the lines
+        lines = get_lines(corners)
+
+        # Create the 2D Path for the mesh
+        path = trimesh.path.path.Path2D(
+            entities=lines,
+            vertices=corners,
+        )
+
+        height = get_height(element)
+
+        # Get the mesh for that building
+        mesh = path.extrude(height=height)
+        
+        # Add that to the list of all the elements
+        buildings.append(mesh)
+    
+    # Combine the meshes into one just so that we can use it as a singular mesh
+    combined_mesh = trimesh.util.concatenate(buildings)
+
+    # Export it out in glb format
+    combined_mesh.export("combined.glb") 
+
+    data_roads = None
+    # Read in the json data
+    with open(STREET_JSON, "r") as f:
+        data_roads = json.load(f)
+    
+    buildings_roads = []
+    buildings_roads.append(combined_mesh)
+
+    for i, road in enumerate(data_roads["elements"]):
+        nodes = get_corners(road)
+
+        path = shapely.geometry.LineString(nodes)
+
+        width = get_width(road)
+
+        street = path.buffer(width)
+
+        mesh = trimesh.creation.extrude_polygon(street, height=0.2)
+
+        buildings_roads.append(mesh)
+    
+    combined_mesh = trimesh.util.concatenate(buildings_roads)
+
+    # Export it out in glb format
+    combined_mesh.export("everything.glb") 
+    
+
+
+if __name__ == "__main__":
+    main()
