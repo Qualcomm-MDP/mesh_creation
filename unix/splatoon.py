2f0898bf608efc8ecd25a37d77e2284a8a078136
Dervin Tian <dervint@umich.edu>
2026-02-10 06:44:09 -0500

    Commented out splatoon

--- unix/splatoon.py
+++ unix/splatoon.py
@@ -5,18 +5,20 @@ from scipy.spatial.transform import Rotation as R
 import cv2
 import math
 import numpy as np
-import matplotlib.pyplot as plt
 
+# A ray casting method that we could use to evaluate the quality of the mesh with images, like mapillary ones
+
+# Define hyperparameters to be used to test an image on it
 SCALE = 5
-CAMERA_LOC = (42.29228, -83.71637)
+CAMERA_LOC = (42.29228, -83.71637) # In GPS coordinates
 HEADING = 135
-INPUT_JSON = "osm_data_buildings.json" 
-INPUT_IMG = "wraps/IMG_6915.JPG"
-radius = 0.5  # adjust size
-interval = 100
+INPUT_JSON = "osm_data_buildings.json" # Get the JSON building for the bounding box
+INPUT_IMG = "wraps/IMG_6915.JPG" # The image that we want to test our mesh against
+radius = 0.5  # adjust size for the balls
+interval = 100 # How many pixels we wold like to skip in the original image
 
 splatter_img = cv2.imread(INPUT_IMG)
-splatter_img = cv2.cvtColor(splatter_img, cv2.COLOR_BGR2RGB)
+splatter_img = cv2.cvtColor(splatter_img, cv2.COLOR_BGR2RGB) # Convert to RGB format
 height, width, _ = splatter_img.shape
 print(height, width)
 
@@ -24,79 +26,105 @@ print(height, width)
 with open(INPUT_JSON, "r") as f:
     data_buildings = json.load(f)
 
+# Set the bounds
 MAX_LAT = float(data_buildings["max_lat"])
 MIN_LAT = float(data_buildings["min_lat"])
 MAX_LON = float(data_buildings["max_lon"]) 
 MIN_LON = float(data_buildings["min_lon"])
 
+# Calculate the converted coordinates to place the camera at
 converted_max_lat = int(MAX_LAT * 10**SCALE)
 converted_max_lon = int(MAX_LON * 10**SCALE)
 converted_min_lon = int(MIN_LON * 10**SCALE)
 converted_min_lat = int(MIN_LAT * 10**SCALE)
 
+# Converted the coordinates for the camera
 converted_cam_lat = int(CAMERA_LOC[0] * 10**SCALE)
 converted_cam_lon = int(CAMERA_LOC[1] * 10**SCALE)
 
+# Get the local coordinates by offsetting it with the bounding boxes
 local_cam_lat = converted_cam_lat - converted_min_lat
 local_cam_lon = converted_cam_lon - converted_min_lon
 
+# Load in the street mesh
 street_mesh = trimesh.load_mesh("combined.glb")
 
+# Create a trimesh scene
 scene = trimesh.Scene()
-scene.add_geometry(street_mesh)
+scene.add_geometry(street_mesh) # Add in the pre-existing street mesh
 
 # Example ray
-ray_origin = np.array([[local_cam_lat, local_cam_lon, -1.83]])
-ray_length = 100.0                     # how far to draw the ray
+ray_origin = np.array([[local_cam_lat, local_cam_lon, -1.83]]) # Start of the ray, where the picture was taken (ignore the negative sign, all meshes were inverted so they were extruded to a negative height to account for that)
+ray_length = 100.0                     # how far to draw the ray (visualization purposes)
 
-FOCAL_LENGTH = 3165
-HOR_FOV = math.atan((width / 2) / FOCAL_LENGTH)
-VERT_FOV = math.atan((height / 2) / FOCAL_LENGTH)
+FOCAL_LENGTH = 3165 # Focal length for apple camera, kinda estimated with chat and using the specs fond online: https://support.apple.com/en-us/111831
+HOR_FOV = math.atan((width / 2) / FOCAL_LENGTH) # Calculate the horizontal FOV
+VERT_FOV = math.atan((height / 2) / FOCAL_LENGTH) # Calculate the vertical FOV
 
+# Calculate the heading angles for the rays that we want to cast out
 MAX_HEADING = math.radians(HEADING) + HOR_FOV
 MIN_HEADING = math.radians(HEADING) - HOR_FOV
 
+# Calculate the tilt for the camera, since we want the rays to cover like everything, so we need to sweep and tilt
 MAX_TILT = VERT_FOV
 MIN_TILT = -1 * VERT_FOV
 
+# Places to store the rays, where they intersect, as well as their color
 rays = []
 locations_hit = []
 colors = []
 
+# How much we will be incrementing our rays by to sweep and cover the area with (heading and tilt)
 delta_heading = (HOR_FOV * 2) / int(width / interval)
 delta_tilt = (VERT_FOV * 2) / int(height / interval)
-height_center = int(height / 2)
+height_center = int(height / 2) # Get the center of the image, will be useful later
 print(height_center)
 
-heading = MIN_HEADING
+heading = MIN_HEADING # Set the current starting position to be the min heading, leftmost so that we sweep from left to right
 for i in range(0, width, interval):
+     # Calculate the unit vector direction of the ray
     x = math.cos(heading)
     y = math.sin(heading)
-    tilt = 0
+    tilt = 0 # Get the tilt
+
+    # Define some data structures since we need to store the colors in 2D ( or at least that is what I found to work as collapsing it lost valuable spatial info )
     column_colors = []
     column_rays = []
+
+    # Crucial, angles change the difference in height for the color pixels, rays shot out with a smaller tilt angle will be closer together basically, so we need to select the colors appropriately
     height_offset = 0 # Want to calculate the height offset for the pixels as we go down x number of degrees as it varies
     focal_length_adj = FOCAL_LENGTH / math.cos(heading) # Tells us how long our baseline segment is
+
+    # Go through the tilts, only need to calculate half and then can just mirror ir
     for j in range(0, int(height / 2), interval):
-        tilt = math.sin(tilt)
-        ray_direction_up = np.array([[x, y, tilt]])
+        tilt = math.sin(tilt) # Get the tilt angle
+        ray_direction_up = np.array([[x, y, tilt]]) # Create rays that tilt both positive and negative
         ray_direction_down = np.array([[x, y, -1 * tilt]])
-        column_rays.append(ray_direction_up)
+        column_rays.append(ray_direction_up) # Store those rays' directions so that we can create them later
         column_rays.append(ray_direction_down)
+
+        # If ray hits a part of the image that is in scope, get that color,
         if height_center + height_offset < height and i < width:
             column_colors.append(splatter_img[height_center + height_offset][i])
             column_colors.append(splatter_img[height_center - height_offset][i])
-        else:
+        else: # Otherwise just grab blac, or some other null color
             column_colors.append([0, 0, 0])
             column_colors.append([0, 0, 0])
+        
+        # Update the magnitude tilt angle of the rays
         tilt += delta_tilt
-        height_offset = abs(int((focal_length_adj * math.sin(tilt)) / (math.sin((math.pi / 2) - tilt))))
+        height_offset = abs(int((focal_length_adj * math.sin(tilt)) / (math.sin((math.pi / 2) - tilt)))) # Calculate the height offset for the next color we want to grab, relative to the center of the image (law of sine)
+    # After shooting out all the tilts of the rays at a given heading, add those colors and those rays and store them
     colors.append(column_colors)
     rays.append(column_rays)
-    heading += delta_heading
+    heading += delta_heading # Update the heading
 
+# Want a place to store the colors that actually are relevant, the ones that are touching the mesh
 hit_colors = []
+
+# Go through the rays
 for i, column_ray in enumerate(rays):
+    # Same deal as before, works best with a 2D data structure to keep track of the colors and the rays
     column_locations = []
     column_hit_colors = []
     for j, ray in enumerate(column_ray):
@@ -106,36 +134,48 @@ for i, column_ray in enumerate(rays):
         # Create a line for the ray
         ray_line = trimesh.load_path(np.vstack([ray_origin, ray_end]))
 
-        # Scene
+        # Optional ability to visualize out the rays that are being created, but takes up a lot of space and is expensive
         # scene.add_geometry(ray_line)
 
+        # Calculate the intersection with the mesh, only consider the first intersection
         locations, index_ray, index_tri = street_mesh.ray.intersects_location(
             ray_origins=ray_origin,
             ray_directions=ray,
             multiple_hits=False
         )
+
+        # If there is an intersection, add that location as well as the color associated with that ray that intersected the mesh
         if len(locations) != 0:
             column_locations.append(locations)
             column_hit_colors.append(colors[i][j])
+
+    # Store the locations and their colors
     locations_hit.append(column_locations)
     hit_colors.append(column_hit_colors)
 
+# Optional to use a point cloud instead of the spheres, but the point clouds can't have color, which is essential
 # locations_hit = np.array(locations_hit)
 # colors = np.array([[255, 0, 0]]) * len(locations_hit)
 # point_cloud = trimesh.points.PointCloud(locations_hit, colors=colors)
 # scene.add_geometry(point_cloud)
 
+# Go thorugh the locations that were intersected
 for i, column in enumerate(locations_hit):
     for j, ray_sent_out in enumerate(column):
-        for loc in ray_sent_out:
+        for loc in ray_sent_out: # For each ray that we casted and hit the mesh
             # Create a small UV sphere
             sphere = trimesh.creation.uv_sphere(radius=radius)
 
+            # Convert the color into RGBA format
             rgba = np.hstack([hit_colors[i][j], 255])
-            # Optional: assign color
+
+            # Assign the color to that sphere
             sphere.visual.vertex_colors[:] = np.tile(rgba, (len(sphere.vertices),1))
 
+            # Place the sphere into the place that we want it
             sphere.apply_translation([loc[0], loc[1], loc[2]])
+
+            # Add it to the scene
             scene.add_geometry(sphere)
 
 # Create a small UV sphere
@@ -144,9 +184,13 @@ sphere = trimesh.creation.uv_sphere(radius=radius)
 # Optional: assign color
 sphere.visual.vertex_colors[:] = [255, 0, 0, 255]  # Red RGBA
 
+# Add sphere to the scene. The red sphere marks the spot in which the image was taken from
 sphere.apply_translation([local_cam_lat, local_cam_lon, -1.83])
 scene.add_geometry(sphere)
 
+# Show the scene using trimesh's built-in show
 scene.show()
+
+# Optionally can export if we want to
 # scene.export("scene.glb")
 # print("Scene exported successfuly!")
